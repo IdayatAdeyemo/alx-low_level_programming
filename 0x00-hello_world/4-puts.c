6ab8944ef3e0c27386972ea0f31d30d7354e3700
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-06-30 15:43:00 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,10 @@
  *main - entry point
  *
   *Return: aways 0 (success)
-  *
+  
  */
 int main(void)
-
 {
-
-puts("\"Programming is like building a multilingual puzzle");
-     
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
