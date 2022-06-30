315e75c9db67d30fe51b30eb08a26f4bbeacfef4
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-06-30 14:45:54 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-printf("Programming is like building a multilingual puzzle\n")
+#include <stdio.h>
+/* 
+ *main - entry point
+ *
+  *Return: aways 0 (success)
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle");
+	return (0);
+}
