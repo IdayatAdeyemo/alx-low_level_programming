1fed5662c64b3b1458d1a10cc3bb0823b7c5ccef
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-01 22:45:55 +0000

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - entry point
+*
+*Return: Always 0 (success)
+*
+*/
+int main(void)
+{
+	char low = 'a';
+
+	while (low <= 'z')
+	{
+		putchar("%c", low);
+		low++
+	}
+	putchar("\n");
+	return (0);
+}
+
