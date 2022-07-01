80ecc602750cc5587779f8c628d3e7254b66e171
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-01 21:08:52 +0000

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,12 +16,20 @@
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-	else if
+	}
+	else 
+	{
 		if (n == 0)
+		{
 		printf("%d is zero\n", n);
+		}
 	else
+	{
 		if (n < 0)
+		{
 		printf("%d is negative\n", n);
+		}}}
 	return (0);
 }
