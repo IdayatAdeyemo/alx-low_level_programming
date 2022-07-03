81add390dc8db33d21d7e65799d0355c271d7326
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-03 14:43:29 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h)
+#include <time.h>
+
+/**
+*main - entry point
+*
+*Return: value 0 (success)
+*
+*/
+
+int main(void)
+{
+	char low;
+	char sp = ' ';
+
+
+	for (low = 'a'; a <= 'z'; low++)
+	{
+		if ((low = 'e)' || (low = 'q'))
+		{
+			putchar(sp);
+		}
+		else
+		{
+			putchar(low);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
+
