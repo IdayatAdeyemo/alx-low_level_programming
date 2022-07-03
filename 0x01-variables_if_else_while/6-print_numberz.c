9c5c08735ad7e70e72a69f7d11208769836380f0
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-03 12:22:47 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+*main Entry - point
+*
+* Return: value 0 (success)
+*/
+
+
+	int main(void)
+		{
+			int a;
+
+				for (a = 0; a <= 9; a++)
+				{
+				putchar(a);
+				}
+					putchar('\n');
+
+		return (0);
