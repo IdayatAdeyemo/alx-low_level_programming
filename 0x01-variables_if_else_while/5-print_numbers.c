cf8a84de0eef6b3b0fddb631d909ea44cc932051
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-03 14:00:57 -0700

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*
+* Return: value 0 (sucess)
+*
+*/
+
+int main(void)
+{
+	int b;
+
+	for(b = 0; b <= 9; b++)
+	{
+		printf("%d", b);
+	}
+	printf("\n");
+	return (0);
+}
