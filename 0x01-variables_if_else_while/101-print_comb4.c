ea5a847b58b3edc0b9746b7e248d5154c7d6de2e
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-03 18:33:50 -0700

    t10

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*
+*Return: value 0 (success)
+*
+*/
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = j + 1; j <= 9; j++)
+		{
+			for (k = k + 1; k <= 9; k++)
+			{
+				putchar(ijk);
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
