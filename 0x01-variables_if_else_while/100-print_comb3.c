4479cf0d869ab60317dead3109231bcf0348e937
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-04 08:41:43 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,8 @@
 
 int main(void)
 {
-	int d,p;
+	int d, p;
+
 	for (d = '0'; d < '9'; d++)
 	{
 		for (p = d + 1; p <= '9'; p++)
@@ -23,12 +24,12 @@ int main(void)
 				if (d == '8' && p == '9')
 					continue;
 
-				putchar (',');
+				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
