f6908ccd0cb46beac1226567804404d62ab6a488
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-04 00:23:59 -0700

    t9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point
+*
+*Return: value 0 (success)
+*
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		if (num == 57)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
