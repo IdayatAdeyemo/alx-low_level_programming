9b35bf38d5674eab5e6e66369ed633860911d88c
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-04 08:18:30 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,16 +13,28 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i < '9'; i++)
 	{
-		for (j = j + 1; j <= 9; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
-			for (k = k + 1; k <= 9; k++)
+			for (k = k + 1; k <= '9'; k++)
 			{
-				putchar(i,j,k);
+				if ((p != d) != q)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == '7' && p == '8')
+						continue;
+
+					putchar(';');
+					putchar(' ');
+				}
 			}
 		}
 	}
+	putchar('\n');
+
+	return (0);
 }
-putchar('\n');
-return (0);
+
