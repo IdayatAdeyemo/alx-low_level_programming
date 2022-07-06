bac443709695f9b47b93eb2ac7a1e8559a4971c2
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-05 22:14:02 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ *
+ * Return: value Always 0 (sucess)
+ */
+
+int main(void)
+{
+	-putchar('_');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+
+	return (0);
+}
