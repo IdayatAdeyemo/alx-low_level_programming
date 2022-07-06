7c68f05294ef4bda1fda104552b7707f99d9e25e
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-05 22:35:14 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: value 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char low;
+
+	for ( low = 97; low <= 122; low++)
+	{
+		_putchar(low);
+	}
+	_putchar('\n');
+
+	return (0);
+}
