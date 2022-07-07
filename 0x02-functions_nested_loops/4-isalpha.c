014e35424e9e90d8a4926bd3fbb528216da2ae9c
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-06 17:56:01 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -14,9 +14,9 @@ int _isalpha(int c)
 
 	for (lower = 'a'; lower <= 'z'; lower++)
 	{
-		for (upper = 'A'; uppercase <= 'Z'; upper++)
+		for (upper = 'A'; upper <= 'Z'; upper++)
 		{
-		       	if ( c == lower) || (c == upper)
+		       	if (c == lower || c == upper)
 			{
 				islet = 1;
 			}
