485a81f47e78649c17bb900db5280f7ecec13b5b
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-06 17:52:45 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**_isalpha - check Holberton
+*@c: an input character
+*decription: function return 1 if the character is a letter,lowercase or uppercase.
+Return: 1 or 0 in otherwise.
+*
+*/
+
+int _isalpha(int c)
+{
+	char lower, upper;
+	int islet = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; uppercase <= 'Z'; upper++)
+		{
+		       	if ( c == lower) || (c == upper)
+			{
+				islet = 1;
+			}
+		}
+	}
+
+	return (islet);
+}
+
