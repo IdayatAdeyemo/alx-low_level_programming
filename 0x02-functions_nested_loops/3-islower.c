549e0a9578ef3fb765b28751daadaf44caaa67ce
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-06 17:03:00 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,12 @@
  *
  * decription:return 1 if c is lowercase
  * return 0 if c is uppercase
- *
+ */
 
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-return (1);
-else
-return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
