d5c92f2923f3fcfe54afca6b042dd88e0a18d3e1
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-08 18:55:27 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 
-		else if (i % 3 == 0)
+		else if ((i % 3 == 0)
 			printf("Fizz");
 
-		else if (i % 5 == 0)
+		else if ((i % 5 == 0)
 			printf("Buzz");
 
 		else
 			printf("%i", i);
-		if (i < 100)
-			printf(" ");
 	}
 
 	printf("\n");
