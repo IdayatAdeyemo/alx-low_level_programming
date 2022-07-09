ecff9eae467e47088f6b1739ec56e0b050e79663
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-08 17:51:56 -0700

    g

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *print_numbers - print 0123456789
  *
@@ -13,7 +12,7 @@ void print_number(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
