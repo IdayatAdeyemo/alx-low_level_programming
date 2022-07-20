038c94150d6e09e6fa6d6be63e48ca3293e3ef17
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-19 17:14:32 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,12 @@ char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
-		if(*s != c)
+		if (*s != c)
 			s++;
 		else
 			return (s);
 	}
-	if(c == '\0')
+	if (c == '\0')
 		return (s);
 
 	return (NULL);
