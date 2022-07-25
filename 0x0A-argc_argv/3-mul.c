f48e7f25e1fbecf0e1716a0a045f1423e167c375
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-25 16:50:42 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc:argument count
+ * @argv:print all argument entries
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul = 1;
+	int i;
+
+	if (argc > 1)
+	{
+	for (i = 1; i < argc; i++)
+	{
+		mul = mul * atoi(argv[i]);
+	}
+	printf("mul = %d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
+
+
