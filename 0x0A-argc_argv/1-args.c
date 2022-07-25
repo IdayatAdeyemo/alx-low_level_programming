3bf9a44d4e7e2c6f6252f53ddecf6e40b4ed11a1
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-07-25 15:00:24 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * @argc:print number of arguments passed
+ * @argv:void
+ *
+ * Return: always 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
