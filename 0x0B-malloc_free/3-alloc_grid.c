e58c6f8b65d45fd672db1c0ddd82e89aad1061d8
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-08-07 04:58:31 -0700

    3 alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,16 +28,14 @@ for (i = 0; i < height; i++)
 {
 grid[i] = malloc(sizeof(int) * width);
 if (grid[i] == NULL)
-{
-for (i = i - 1; i >= 0; i--)
-{
-free(grid[i]);
 }
+{
 free(grid);
 return (NULL);
 }
-}
-for (i = 0; j < width; j++)
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
 grid[i][j] = 0;
 return (grid);
 }
