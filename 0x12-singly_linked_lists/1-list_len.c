a13c68bbdb27b7cc9b1ac67dd5df98d0e6f7b2b7
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-08-11 14:41:47 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,10 +11,9 @@ size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 	
-	while (h != NULL)
+	if (h != NULL)
 	{
-		n++;
-		h = h->next;
+		return ((n + 1) + list_len(h->next));
 	}
 	return (n);
 }
