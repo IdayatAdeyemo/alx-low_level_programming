d6ac2870c53b4ed2f698528e647f6e6212be5a73
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-08-11 15:19:45 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -23,9 +23,9 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->next = len;
+	new->len = len;
 	new->next = (*head);
 	(*head) = new;
 
-	return (**head);
+	return (*head);
 }
