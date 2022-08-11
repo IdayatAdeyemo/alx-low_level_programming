9e6e956ea777eb4ce6bf308adbf078fcc5f81a39
IdayatAdeyemo <Adeyemo3389@gmail.com>
2022-08-11 14:33:18 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * list_len - List elements in a list
+ * @h: Header node
+ *
+ * Return: The count
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	if (h != NULL)
+	{
+		return ((n + 1) + list_len(h->next));
+	}
+	return (n);
+}
